e38389c24b92f84dc50c040114bcf12b4152b05b
anastasia <anastasia.gribovskaia@gmail.com>
2023-03-16 02:00:33 +0300

    refactor: apply vitest, testing-library and react-router

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,25 @@
+import React from "react";
+import { HashRouter, Route, Routes } from 'react-router-dom';
+import Home from './pages/Home';
+import NotFound from './pages/NotFound';
+
 function App() {
   return (
     <div>
-      Hello world!
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
     </div>
   );
 }
 
-export default App;
+function WrappedApp() {
+  return (
+    <HashRouter>
+      <App />
+    </HashRouter>
+  )
+}
+
+export default WrappedApp;
