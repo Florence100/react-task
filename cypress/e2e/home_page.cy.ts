ac8436935c3c8276786b6bfef125e24fe0c5dad2
anastasia <anastasia.gribovskaia@gmail.com>
2023-04-26 18:02:08 +0300

    fix: fix an error in the coverage report

--- cypress/e2e/home_page.cy.ts
+++ cypress/e2e/home_page.cy.ts
@@ -1,6 +1,11 @@
-describe('The Home Page', () => {
+/// <reference types="Cypress" />
+describe('The Form Page', () => {
     it('the h3 contains the correct text', () => {
       cy.visit('/user');
       cy.get("h3").eq(0).should("have.text", "Пожалуйста, заполните форму");
+      cy.get("h3").eq(1).should("have.text", "У вас пока еще нет карточек");
+    })
+    it('Just a test to remove page load on coverage saving', () => {
+      expect(true).to.equal(true);
     })
 })
